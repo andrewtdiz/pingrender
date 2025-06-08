8df7ca042f32e0000d9d45860b1b973aa0fe46a2
andrewtdiz <59515127+andrewtdiz@users.noreply.github.com>
2025-06-08 15:07:32 -0500

    first

---
+++ index.ts
@@ -0,0 +1,25 @@
+const url = "https://eval-sheets.onrender.com/ping";
+
+const ping = () =>
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        console.log(response);
+        console.warn("Network response was not ok");
+      } else console.log("Ping successful");
+    })
+    .catch((error) => {
+      console.error("Error pinging endpoint:", error);
+    });
+
+function startPinging() {
+  console.log("Pinging endpoint...");
+  ping();
+
+  setInterval(() => {
+    console.log("Pinging endpoint...");
+    ping();
+  }, 5 * 60 * 1000);
+}
+
+startPinging();
