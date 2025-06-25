c873a747c5c486c0f7d4b46fb7ac774028a2d36b
Andrew DiZenzo <59515127+andrewtdiz@users.noreply.github.com>
2025-06-24 18:29:54 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,37 +1,38 @@
 const formatDate = (date: Date) => {
-  return date.toLocaleString('en-US', {
-    year: 'numeric',
-    month: 'short',
-    day: 'numeric',
-    hour: '2-digit',
-    minute: '2-digit',
-    second: '2-digit',
-    hour12: true
-  });
-};
-
-const url = process.env.URL ?? "";
-
-const ping = () =>
-  fetch(url)
-    .then((response) => {
-      if (!response.ok) {
-        console.log(`[${formatDate(new Date())}]`, response);
-        console.warn(`[${formatDate(new Date())}] Network response was not ok`);
-      } else console.log(`[${formatDate(new Date())}] Ping successful`);
-    })
-    .catch((error) => {
-      console.error(`[${formatDate(new Date())}] Error pinging endpoint:`, error);
+    return date.toLocaleString('en-US', {
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: true
     });
+  };
 
-function startPinging() {
-  console.log(`[${formatDate(new Date())}] Pinging endpoint...`);
-  ping();
-
-  setInterval(() => {
+  const ping = (url: string) =>
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          console.log(`[${formatDate(new Date())}]`, response);
+          console.warn(`[${formatDate(new Date())}] Network response was not ok`);
+        } else console.log(`[${formatDate(new Date())}] Ping successful`);
+      })
+      .catch((error) => {
+        console.error(`[${formatDate(new Date())}] Error pinging endpoint:`, error);
+      });
+  
+  function startPinging() {
     console.log(`[${formatDate(new Date())}] Pinging endpoint...`);
-    ping();
-  }, 5 * 60 * 1000);
-}
-
-startPinging();
+    ping(process.env.URL ?? "");
+    ping("https://elevenmcp.onrender.com/ping");
+  
+    setInterval(() => {
+      console.log(`[${formatDate(new Date())}] Pinging endpoint...`);
+      ping(process.env.URL ?? "");
+      ping("https://elevenmcp.onrender.com/ping");
+    }, 5 * 60 * 1000);
+  }
+  
+  startPinging();
+  
